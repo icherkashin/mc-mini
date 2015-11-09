76d720447ae68302f533a998022fa3f406c695bd
Ivan Cherkashin <cherkashin.i.a@gmail.com>
2015-11-08 19:07:13 -0800

    Remove redundant and unnecessary lines from main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
-#include <iomanip>
-#include <cassert>
 
-#include "matrixForms/sparseForms.h"
-#include "matrixForms/denseForms.h"
 #include "geometry/geometry.h"
 #include "problem/problem.h"
 #include "output/output.h"
 #include "parser/parser.h"
 
-using namespace Eigen;
-using namespace std;
-
 int main(int argc, char ** argv) {
 
   if (argc == 1) {
-    cerr << "usage: " << string{argv[0]} << " <parameter file>." << endl << endl;
-    exit (-1);
+    throw std::invalid_argument("usage: " + std::string{argv[0]} + " <parameter file>.");
   }
-
-  setNbThreads(16);
 
   ParamParser parser(string{argv[1]});
   GeometryStructure geometry (parser);
@@ -28,25 +18,16 @@ int main(int argc, char ** argv) {
 
   problem.initializeProblem();
   
-  problem.updateForcingTerms();
-  problem.solveStokes();
-
-  problem.recalculateTimestep();
-  
-  do {    
-    output.writeHDF5File (problem.getTimestepNumber());
-    cout << "<Timestep: " << problem.getTimestepNumber() << "; t=" << problem.getTime() << ">" << endl << endl;
+  do 
+  {      
+    std::cout << "<Timestep: " << problem.getTimestepNumber() << "; t=" << problem.getTime() << ">" << std::endl;
     problem.updateForcingTerms();
     problem.solveStokes();
     problem.recalculateTimestep();
     problem.solveAdvectionDiffusion();
-
-  } while (problem.advanceTimestep());
-
-  output.writeHDF5File (problem.getTimestepNumber());
-  cerr << "Timestep: " << problem.getTimestepNumber() << "; t = " << problem.getTime() << endl;
-
-  output.writeHDF5File();
-  
+    output.outputData (problem.getTimestepNumber());
+  } 
+  while (problem.advanceTimestep());
+ 
   return 0;
 }
