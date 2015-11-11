083e4262227fc1b5d2bbeda329453f4ceb5fb3cf
Ivan Cherkashin <cherkashin.i.a@gmail.com>
2015-11-11 14:31:53 -0800

    Remove trailing spaces.

--- source/output/output.cpp
+++ source/output/output.cpp
@@ -23,7 +23,7 @@ OutputStructure::OutputStructure (ParamParser&       pp,
     parser   (pp),
     geometry (gs),
     problem  (ps) {
-  M  = geometry.getM(); 
+  M  = geometry.getM();
   N  = geometry.getN();
   dx = problem.getH();
 
@@ -41,7 +41,7 @@ OutputStructure::OutputStructure (ParamParser&       pp,
     sprintf(s, "mkdir %s", outputPath.c_str());
     if (system(s) != 0) {
       throw std::runtime_error("<Error: couldn't create directory " + outputPath + ">");
-    }    
+    }
   }
 
   problemXdmfFile.open ((outputPath + "/" + outputFilename + "-series.xdmf").c_str(), ofstream::out);
@@ -84,16 +84,16 @@ void OutputStructure::writeHDF5File (const int timestep) {
                   << "            " << dx << " " << dx << endl
                   << "          </DataItem>" << endl
                   << "        </Geometry>" << endl;
-  
+
   hid_t dataset, datatype, dataspace;
   herr_t status;
   hsize_t dimsf[2];
   dimsf[0] = M; dimsf[1] = N;
-  
+
   dataspace = H5Screate_simple (2, dimsf, NULL);
   datatype = H5Tcopy (H5T_NATIVE_DOUBLE);
   status = H5Tset_order (datatype, H5T_ORDER_LE);
-  
+
   if (status == -1) {
       throw std::runtime_error("<H5Tset_order failed::" + boost::lexical_cast<std::string>(__FILE__) + ":" + boost::lexical_cast<std::string>(__LINE__) + ">");
   }
@@ -101,10 +101,10 @@ void OutputStructure::writeHDF5File (const int timestep) {
   // Write temperature
   dataset = H5Dcreate2(outputFile, "Temperature", datatype, dataspace,
                        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  
+
   status = H5Dwrite (dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
                      H5P_DEFAULT, geometry.getTemperatureData());
- 
+
   if (status == -1) {
       throw std::runtime_error("<H5Dwrite failed::" + boost::lexical_cast<std::string>(__FILE__) + ":" + boost::lexical_cast<std::string>(__LINE__) + ">");
   }
@@ -119,11 +119,10 @@ void OutputStructure::writeHDF5File (const int timestep) {
   dataset = H5Dcreate2(outputFile, "Pressure", datatype, dataspace,
                        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
   status = H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
-                     H5P_DEFAULT, geometry.getPressureData());  
+                     H5P_DEFAULT, geometry.getPressureData());
 
   if (status == -1) {
       throw std::runtime_error("<H5Dwrite failed::" + boost::lexical_cast<std::string>(__FILE__) + ":" + boost::lexical_cast<std::string>(__LINE__) + ">");
-    
   }
 
   problemXdmfFile << "        <Attribute Name=\"Pressure\" AttributeType=\"Scalar\" Center=\"Cell\">" << endl
@@ -131,32 +130,31 @@ void OutputStructure::writeHDF5File (const int timestep) {
                   << "            " << outputFilename + "-" + boost::lexical_cast<std::string> (timestep) << ".h5:/Pressure" << endl
                   << "          </DataItem>" << endl
                   << "        </Attribute>" << endl;
-   
+
   // Write U Velocity
   static double * interpolatedUVelocityData = new double [M * N];
-  static DataWindow<double> interpolatedUVelocityWindow (interpolatedUVelocityData, N, M);      
-  static DataWindow<double> uVelocityBoundaryWindow (geometry.getUVelocityBoundaryData(), 2, M);   
+  static DataWindow<double> interpolatedUVelocityWindow (interpolatedUVelocityData, N, M);
+  static DataWindow<double> uVelocityBoundaryWindow (geometry.getUVelocityBoundaryData(), 2, M);
   static DataWindow<double> uVelocityWindow (geometry.getUVelocityData(), N - 1, M);
 
-  
   for (int i = 0; i < M; ++i) {
     for (int j = 0; j < N; ++j) {
       if (j == 0) {
         interpolatedUVelocityWindow (j, i) = (uVelocityBoundaryWindow (0, i) +
-                                              uVelocityWindow         (0, i)) / 2;                                
+                                              uVelocityWindow (0, i)) / 2;
       } else if (j == (N - 1)) {
-        interpolatedUVelocityWindow (j, i) = (uVelocityWindow         (N - 2, i) +
+        interpolatedUVelocityWindow (j, i) = (uVelocityWindow (N - 2, i) +
                                               uVelocityBoundaryWindow (1, i)) / 2;
       } else {
         interpolatedUVelocityWindow (j, i) = (uVelocityWindow (j - 1, i) +
-                                              uVelocityWindow (j, i)) / 2;			      	      
+                                              uVelocityWindow (j, i)) / 2;
       }
     }
   }
 
   dataset = H5Dcreate2(outputFile, "UVelocity", datatype, dataspace,
                        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  
+
   status = H5Dwrite (dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
                      H5P_DEFAULT, interpolatedUVelocityData);
 
@@ -168,44 +166,45 @@ void OutputStructure::writeHDF5File (const int timestep) {
                   << "          <DataItem Dimensions=\"" << M << " " << N << "\" NumberType=\"Float\" Precision=\"8\" Format=\"HDF\">" << endl
                   << "            " << outputFilename + "-" + boost::lexical_cast<std::string> (timestep) << ".h5:/UVelocity" << endl
                   << "          </DataItem>" << endl
-                  << "        </Attribute>" << endl; 
-    
+                  << "        </Attribute>" << endl;
+
   // Write V Velocity
   static double * interpolatedVVelocityData = new double [M * N];
-  static DataWindow<double> interpolatedVVelocityWindow (interpolatedVVelocityData, N, M);  
-  static DataWindow<double> vVelocityBoundaryWindow (geometry.getVVelocityBoundaryData(), N, 2);  
-  static DataWindow<double> vVelocityWindow (geometry.getVVelocityData(), N, M - 1);  
-  
+  static DataWindow<double> interpolatedVVelocityWindow (interpolatedVVelocityData, N, M);
+  static DataWindow<double> vVelocityBoundaryWindow (geometry.getVVelocityBoundaryData(), N, 2);
+  static DataWindow<double> vVelocityWindow (geometry.getVVelocityData(), N, M - 1);
+
   for (int i = 0; i < M; ++i) {
     for (int j = 0; j < N; ++j) {
       if (i == 0) {
         interpolatedVVelocityWindow (j, i) = (vVelocityBoundaryWindow (j, 0) +
-                                              vVelocityWindow         (j, 0)) / 2;                                      
+                                              vVelocityWindow (j, 0)) / 2;
       } else if (i == (M - 1)) {
-        interpolatedVVelocityWindow (j, i) = (vVelocityWindow         (j, M - 2) +
-                                              vVelocityBoundaryWindow (j, 1)) / 2; 						    
+        interpolatedVVelocityWindow (j, i) = (vVelocityWindow (j, M - 2) +
+                                              vVelocityBoundaryWindow (j, 1)) / 2;
       } else {
-        interpolatedVVelocityWindow (j, i) = (vVelocityWindow         (j, i - 1) +
-                                              vVelocityWindow         (j, i)) / 2;			      
+        interpolatedVVelocityWindow (j, i) = (vVelocityWindow (j, i - 1) +
+                                              vVelocityWindow (j, i)) / 2;
       }
     }
   }
-  dataset = H5Dcreate2(outputFile, "VVelocity", datatype, dataspace, 
+
+  dataset = H5Dcreate2(outputFile, "VVelocity", datatype, dataspace,
                        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
   status = H5Dwrite (dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
                      H5P_DEFAULT, interpolatedVVelocityData);
-  
+
   if (status == -1) {
       throw std::runtime_error("<H5Dwrite failed::" + boost::lexical_cast<std::string>(__FILE__) + ":" + boost::lexical_cast<std::string>(__LINE__) + ">");
-  }
+    }
 
   problemXdmfFile << "        <Attribute Name=\"VVelocity\" AttributeType=\"Scalar\" Center=\"Cell\">" << endl
                   << "          <DataItem Dimensions=\"" << M << " " << N << "\" NumberType=\"Float\" Precision=\"8\" Format=\"HDF\">" << endl
                   << "            " << outputFilename + "-" + boost::lexical_cast<std::string> (timestep) << ".h5:/VVelocity" << endl
                   << "          </DataItem>" << endl
                   << "        </Attribute>" << endl;
-	   
+
   static double * velocityDivergenceData = new double[M * N];
   static DataWindow<double> velocityDivergenceWindow (velocityDivergenceData, N, M);
 
@@ -228,7 +227,7 @@ void OutputStructure::writeHDF5File (const int timestep) {
       } else {
         uDivergence = (uVelocityWindow (j - 1, i) - uVelocityWindow (j, i)) / dx;
       }
-      
+
       velocityDivergenceWindow (j, i) = uDivergence + vDivergence;
     }
   }
@@ -238,7 +237,7 @@ void OutputStructure::writeHDF5File (const int timestep) {
 
   status = H5Dwrite (dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
                      H5P_DEFAULT, velocityDivergenceData);
-  
+
   if (status == -1) {
       throw std::runtime_error("<H5Dwrite failed::" + boost::lexical_cast<std::string>(__FILE__) + ":" + boost::lexical_cast<std::string>(__LINE__) + ">");
   }
@@ -250,7 +249,7 @@ void OutputStructure::writeHDF5File (const int timestep) {
                   << "        </Attribute>" << endl;
 
   dimsf[0] = (M + 1); dimsf[1] = (N + 1);
- 
+
   dataspace = H5Screate_simple (2, dimsf, NULL);
   datatype = H5Tcopy (H5T_NATIVE_DOUBLE);
   status = H5Tset_order (datatype, H5T_ORDER_LE);
@@ -258,7 +257,7 @@ void OutputStructure::writeHDF5File (const int timestep) {
   if (status == -1) {
       throw std::runtime_error("<H5Tset_order failed:: " + boost::lexical_cast<std::string>(__FILE__) + ":" + boost::lexical_cast<std::string>(__LINE__) + ">");
   }
- 
+
   // Write Viscosity
   dataset = H5Dcreate2(outputFile, "Viscosity", datatype, dataspace,
                        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
@@ -280,5 +279,5 @@ void OutputStructure::writeHDF5File (const int timestep) {
   H5Sclose (dataspace);
   H5Tclose (datatype);
   H5Dclose (dataset);
-  H5Fclose (outputFile);  
+  H5Fclose (outputFile);
 }
